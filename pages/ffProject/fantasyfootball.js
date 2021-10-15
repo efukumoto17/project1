9898e1db203289c197d4cb1f26dcbbfb1394850e
evanfukumoto <efukumoto17@gmail.com>
2021-10-15 14:36:30 -0700

    setup FF page

---
+++ pages/ffProject/fantasyfootball.js
@@ -0,0 +1,12 @@
+import Layout from '../../components/layout'
+let FantasyFootball = ({}) => {
+   console.log("hello");
+   return (
+      <Layout>
+         <div> 
+            <h1>Fantasy Football</h1>
+         </div>
+      </Layout>
+   )
+}
+export default FantasyFootball;
