d15788ceaf634538f6e51b9b9890c84c128299c3
evanfukumoto <efukumoto17@gmail.com>
2021-10-18 16:39:25 -0700

    fixed home page and added profile pic

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Evan Fukumoto'
+export const siteTitle = 'Evan\'s Website'
 
 export default function Layout({ children, home }) {
   return (
@@ -30,10 +30,10 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/Evan.jpg"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={150}
+              width={225}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -44,10 +44,10 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/Evan.jpg"
                   className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
+                  height={100}
+                  width={150}
                   alt={name}
                 />
               </a>
